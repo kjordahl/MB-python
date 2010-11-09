606de493d0e953726890704783506498d3ccb98a
Kelsey Jordahl <kjordahl@alum.mit.edu>
2010-11-08 19:06:31 -0500

    mbnav2sql now reads multiple .fnv files listed in a datalist.mb-1 file

--- mbnav2sql.py
+++ mbnav2sql.py
@@ -1,38 +1,30 @@
 #!/usr/bin/env python2.7
 #
-# load MB .fnv file and load into PostGIS database
+# load MB .fnv files from datalist and load into PostGIS database
 #
 
 # Kelsey Jordahl
-# Time-stamp: <Mon Nov  8 17:50:16 EST 2010>
+# Time-stamp: <Mon Nov  8 19:05:21 EST 2010>
 
 import sys
 import psycopg2
 from datetime import datetime, date, time
 
-# testing on specific file
-#file = "/Volumes/Data/multibeam/surveys/LDEO/EW9602/hs_ew9602_120_bcenb.mb24"
-file = "/Users/kels/DOTS/VANC04MV/SIOGDC_VANC04MV_20070727125814012_20070727125814012_SBdespike.2002nov11.truep.mb32"
-infofile = file + ".inf"
-navfile = file + ".fnv"
-print navfile
+#datadir = "/Users/kels/DOTS/VANC04MV/"
+datadir = "/Users/kels/MB-SystemExamples.5.1.0/cookbook_examples/other_data_sets/ew0204survey/"
+datalist = datadir + "datalist.mb-1"
 schema = "multibeam"
 shorttable = "test"
 # concatenate schema.table
 table = schema + "." + shorttable
 
-
-# open the .inf file
 try:
-    f = open(navfile,'r')
-
+    d = open(datalist,'r')
 except:
     exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
     sys.exit("File open failed!\n ->%s" % (exceptionValue))
 
 # connect to PostGIS database
-#conn_string = "host='chipotle' dbname='gis_test' user='kels'"
-# connect local
 conn_string = "host='localhost' dbname='gis_test' user='kels'"
 print "Connecting to database\n	->%s" % (conn_string)
 try:
@@ -55,50 +47,74 @@ sql = "CREATE TABLE " + table + " (file_id SERIAL PRIMARY KEY);"
 cursor.execute(sql);
 sql = "SELECT AddGeometryColumn('" + schema + "','" + shorttable + "','the_geom','4326','GEOMETRY',2);"
 cursor.execute(sql);
-sql = "INSERT INTO " + table + " (file_id, the_geom)"
-#cursor.execute("VALUES (1,ST_Geometry('LINESTRING(")
-
-sql = sql + " VALUES (1,ST_GeomFromText('LINESTRING("
-first = 1;
-
-# parse the file
-for line in f:
+    
+id = 1;
+for line in d:
     # can the type be set in split?
-#    (year,month,day,hour,minute,second,x,lon,lat,q,w,y)=line.split();
-    fields=line.split();
-    if len(fields)>9:
-        year=int(fields[0])
-        month=int(fields[1])
-        day=int(fields[2])
-        hour=int(fields[3])
-        minute=int(fields[4])
-        second=float(fields[5])
+    #    (year,month,day,hour,minute,second,x,lon,lat,q,w,y)=line.split();
+    fields = line.split();
+    file = datadir + fields[0];
+    # testing on specific file
+    #file = "/Volumes/Data/multibeam/surveys/LDEO/EW9602/hs_ew9602_120_bcenb.mb24"
+    #file = "/Users/kels/DOTS/VANC04MV/SIOGDC_VANC04MV_20070727125814012_20070727125814012_SBdespike.2002nov11.truep.mb32"
+    infofile = file + ".inf"
+    navfile = file + ".fnv"
+    print navfile
+
+# break here for testing
+# quit()
+
+    # open the .fbt file
+    try:
+        f = open(navfile,'r')
+
+    except:
+        exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+        sys.exit("File open failed!\n ->%s" % (exceptionValue))
+
+    sql = "INSERT INTO " + table + " (file_id, the_geom)"
+
+    sql = sql + " VALUES (" + str(id) + ",ST_GeomFromText('LINESTRING("
+    first = 1;
+
+    # parse the .fnv file
+    for line in f:
+        fields=line.split();
+        if len(fields)>9:               # minimal error checking
+            year=int(fields[0])
+            month=int(fields[1])
+            day=int(fields[2])
+            hour=int(fields[3])
+            minute=int(fields[4])
+            second=float(fields[5])
         
-        d = date(year, month, day)
-        t = time(hour, minute, int(second)) # second is float, round it
-        lon=float(fields[7])
-        lat=float(fields[8])
+            d = date(year, month, day)
+            t = time(hour, minute, int(second)) # second is float, round it
+            lon=float(fields[7])
+            lat=float(fields[8])
 
-        #    print(hour, minute, second)
-        #    print datetime.combine(d, t), float(lat), float(lon)
-        #    print line,
-        if first:
-            first=0
+            # for testing
+            #    print(hour, minute, second)
+            #    print datetime.combine(d, t), float(lat), float(lon)
+            #    print line,
+            if first:
+                first=0
+            else:
+                sql = sql + ","
+            sql = sql + str(lon) + " " + str(lat)
         else:
-            sql = sql + ","
-        sql = sql + str(lon) + " " + str(lat)
-    else:
-        print len(fields), fields
+            print len(fields), fields
 
-try:
     sql = sql + ")',4326));"
     cursor.execute(sql);
+    id = id + 1
+
+try:
     conn.commit()
     cursor.close()
     conn.close()
 
 except:
-    #    print sql
     print datetime.combine(d, t), float(lon), float(lat)
     
     exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
