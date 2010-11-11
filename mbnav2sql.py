9acfbbd3ba0296c64b218748d93284dd09bc71d2
Kelsey Jordahl <kels@ancho.local>
2010-11-11 16:08:19 -0500

    Rearranged mbnav2sql.pl in functional style

--- mbnav2sql.py
+++ mbnav2sql.py
@@ -1,127 +1,89 @@
 #!/usr/bin/env python2.7
-#
-# load MB .fnv files from datalist and load into PostGIS database
-#
+"""
+load MB .fnv files from datalist and load into PostGIS database
 
-# Kelsey Jordahl
-# Time-stamp: <Thu Nov 11 10:35:47 EST 2010>
+Author: Kelsey Jordahl
+Version: pre-alpha
+Copyright: Kelsey Jordahl 2010
+License: GPLv3
+Time-stamp: <Thu Nov 11 16:05:55 EST 2010>
+
+    This program is free software: you can redistribute it and/or
+    modify it under the terms of the GNU General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.  A copy of the GPL
+    version 3 license can be found in the file COPYING or at
+    <http://www.gnu.org/licenses/>.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+"""
 
 import sys
 import psycopg2
 from datetime import datetime, date, time
 
-#datadir = "/Users/kels/DOTS/VANC04MV/"
-#datadir = "/Users/kels/MB-SystemExamples.5.1.0/cookbook_examples/other_data_sets/ew0204survey/"
-datadir = ""
-datalist = datadir + "datalist.mb-1"
-schema = "multibeam"
-shorttable = "test"
-# concatenate schema.table
-table = schema + "." + shorttable
-badnav = 0
+def main():
+    # TODO: make these defaults and take command line options
+    #datadir = "/Users/kels/DOTS/VANC04MV/"
+    #datadir = "/Users/kels/MB-SystemExamples.5.1.0/cookbook_examples/other_data_sets/ew0204survey/"
+    datadir = ""
+    datalist = datadir + "datalist.mb-1"
+    schema = "multibeam"
+    shorttable = "test"
+    # concatenate schema.table
+    table = schema + "." + shorttable
+    badnav = 0
 
-try:
-    d = open(datalist,'r')
-except:
-    exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-    sys.exit("File open failed!\n ->%s" % (exceptionValue))
-
-# connect to PostGIS database
-conn_string = "host='chipotle' dbname='gis_test' user='kels'"
-print "Connecting to database\n	->%s" % (conn_string)
-try:
-    conn = psycopg2.connect(conn_string)
-    cursor = conn.cursor()
-    print "Connected!\n"
-except:
-    exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-    sys.exit("Database connection failed!\n ->%s" % (exceptionValue)) 
-
-# create the table
-#CREATE SCHEMA multibeam AUTHORIZATION kels;   # if schema doesn't exist
-cursor.execute("BEGIN;")
-sql = "DROP TABLE IF EXISTS " + table + ";"
-print sql
-cursor.execute(sql)
-# for GEOGRAPHY
-#sql = "CREATE TABLE " + table + " (file_id SERIAL PRIMARY KEY, track GEOGRAPHY);"
-# for GEOMETRY
-sql = "CREATE TABLE " + table + " (file_id SERIAL PRIMARY KEY);"
-cursor.execute(sql);
-print sql
-sql = "SELECT AddGeometryColumn('" + schema + "','" + shorttable + "','the_geom','4326','GEOMETRY',2);"
-print sql
-cursor.execute(sql);
-
-id = 1;
-for line in d:
-    # can the type be set in split?
-    #    (year,month,day,hour,minute,second,x,lon,lat,q,w,y)=line.split();
-    fields = line.split();
-    file = datadir + fields[0];
-    # testing on specific file
-    #file = "/Volumes/Data/multibeam/surveys/LDEO/EW9602/hs_ew9602_120_bcenb.mb24"
-    #file = "/Users/kels/DOTS/VANC04MV/SIOGDC_VANC04MV_20070727125814012_20070727125814012_SBdespike.2002nov11.truep.mb32"
-    infofile = file + ".inf"
-    navfile = file + ".fnv"
-    print navfile
-
-# break here for testing
-# quit()
-
-    # open the .fbt file
     try:
-        f = open(navfile,'r')
+        d = open(datalist,'r')
+    except:
+        exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+        sys.exit("File open failed!\n ->%s" % (exceptionValue))
+    
+    # connect to PostGIS database
+    conn_string = "host='chipotle' dbname='gis_test' user='kels'"
+    print "Connecting to database\n	->%s" % (conn_string)
+    try:
+        conn = psycopg2.connect(conn_string)
+        cursor = conn.cursor()
+        print "Connected!\n"
+    except:
+        exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+        sys.exit("Database connection failed!\n ->%s" % (exceptionValue)) 
 
-        sql = "INSERT INTO " + table + " (file_id, the_geom)"
+    # create the table
+    #CREATE SCHEMA multibeam AUTHORIZATION kels;   # if schema doesn't exist
+    cursor.execute("BEGIN;")
+    sql = "DROP TABLE IF EXISTS " + table + ";"
+    print sql
+    cursor.execute(sql)
+    # for GEOGRAPHY
+    #sql = "CREATE TABLE " + table + " (file_id SERIAL PRIMARY KEY, track GEOGRAPHY);"
+    # for GEOMETRY
+    sql = "CREATE TABLE " + table + " (file_id SERIAL PRIMARY KEY);"
+    cursor.execute(sql);
+    print sql
+    sql = "SELECT AddGeometryColumn('" + schema + "','" + shorttable + "','the_geom','4326','GEOMETRY',2);"
+    print sql
+    cursor.execute(sql);
 
-        sql = sql + " VALUES (" + str(id) + ",ST_GeomFromText('LINESTRING("
-#    sql = sql + " VALUES (" + str(id) + ",ST_GeomFromText('POINT("
-        linecount = 0;
+    id = 1;
+    for line in d:
+        # can the type be set in split?
+        #    (year,month,day,hour,minute,second,x,lon,lat,q,w,y)=line.split();
+        fields = line.split();
+        file = datadir + fields[0];
+        infofile = file + ".inf"
+        navfile = file + ".fnv"
+        print navfile
+        sql = parse_fnv(navfile,table,id)
 
-        # parse the .fnv file
-        for line in f:
-            fields=line.split();
-            if len(fields)>9:               # minimal error checking
-                year=int(fields[0])
-                month=int(fields[1])
-                day=int(fields[2])
-                hour=int(fields[3])
-                minute=int(fields[4])
-                second=float(fields[5])
-
-                d = date(year, month, day)
-                t = time(hour, minute, int(second)) # second is float, round it
-                lon=float(fields[7])
-                if lon<0:                   # wrap eastern hemisphere
-                    lon = lon + 360
-                lat=fields[8]
-
-                # for testing
-                #    print(hour, minute, second)
-                #    print datetime.combine(d, t), float(lat), float(lon)
-                #    print line,
-                if linecount == 0:
-                    print "%0.6f %s" % (lon, lat)
-#                sql = sql + "%f %f" % (lon, lat)
-                else:
-                    sql = sql + ","
-                # as floats
-                #            sql = sql + "%f %f" % (lon, lat)
-                # as strings
-                if lon < 1 and (abs(lat) < 1 or lat < -89):
-                    print "Bad nav point"
-                    badnav = badnav + 1;
-                else:
-                    sql = sql + "%s %s" % (lon, lat)
-                    linecount = linecount + 1;
-            else:
-                print len(fields), fields
-
-        sql = sql + ")',4326));"
-        
-        # only insert into database if at least 2 nav points were read
-        if linecount > 1:
+        # only insert into database if valid string is returned
+        if sql:
             try:
                 cursor.execute(sql);
             except:
@@ -129,23 +91,88 @@ for line in d:
                 sys.exit("SQL command failed!\n ->%s" % (exceptionValue))
 
             id = id + 1
-        else:
-            print "only %d line read - not included in database" % linecount
+
+    try:
+        conn.commit()
+        cursor.close()
+        conn.close()
+        print "%d bad nav points ignored" % badnav
 
     except:
+        print datetime.combine(d, t), float(lon), float(lat)
         exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-#        sys.exit("File open failed!\n ->%s" % (exceptionValue))
-        print("File open failed!\n ->%s" % (exceptionValue))
+        sys.exit("Barf!\n ->%s" % (exceptionValue)) 
 
+# end main()
 
-try:
-    conn.commit()
-    cursor.close()
-    conn.close()
-    print "%d bad nav points ignored" % badnav
+def parse_fnv(navfile,table,id):
+    """ Parse a .fnv fast nav file and return a line of SQL code to
+    INSERT into database.  Currently the database name is a global
+    variable, that should change.
+    """
+    # open the .fbt file
+    try:
+         f = open(navfile,'r')
+    except:
+         exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
+         sys.exit("File open failed!\n ->%s" % (exceptionValue))
 
-except:
-    print datetime.combine(d, t), float(lon), float(lat)
-    
-    exceptionType, exceptionValue, exceptionTraceback = sys.exc_info()
-    sys.exit("Barf!\n ->%s" % (exceptionValue)) 
+    sql = "INSERT INTO " + table + " (file_id, the_geom)"
+    sql = sql + " VALUES (" + str(id) + ",ST_GeomFromText('LINESTRING("
+    #    sql = sql + " VALUES (" + str(id) + ",ST_GeomFromText('POINT("
+    linecount = 0;
+
+    # parse the .fnv file
+    for line in f:
+        (lat, lon, t) = get_navpoint(line);
+        if lon < 1 and (abs(lat) < 1 or lat < -89):
+            print "Bad nav point"
+            BADNAV = BADNAV + 1;        # increment GLOBAL bad nav count
+        else:
+            point = "%s %s" % (lon, lat)
+
+        if point:
+            if linecount == 0:
+                sql = sql + point
+            else:
+                sql = sql + "," + point
+            linecount = linecount + 1
+    sql = sql + ")',4326));"
+    if linecount > 1:
+        return sql
+        id = id + 1
+    else:
+        print "only %d line read - not included in database" % linecount
+        return ""
+
+# end parse_fnv
+        
+def get_navpoint(line):
+    """ Parse a line of .fbt file to return longitude, latitude, Python time
+
+    """
+
+    fields=line.split();
+    if len(fields)>9:               # minimal error checking
+        year=int(fields[0])
+        month=int(fields[1])
+        day=int(fields[2])
+        hour=int(fields[3])
+        minute=int(fields[4])
+        second=float(fields[5])
+
+        d = date(year, month, day)
+        t = time(hour, minute, int(second)) # second is float, round it
+        lon=float(fields[7])
+        if lon < 0:                   # wrap eastern hemisphere
+            lon = lon + 360
+        lat=float(fields[8])
+        return (lat, lon, t)
+    else:
+        print len(fields), fields
+        return ()
+
+# end get_navpoint()
+
+if __name__ == '__main__':
+    main()
